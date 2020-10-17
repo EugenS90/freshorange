89d7ecadcb8db45a3ec99d45cdae429efcb4f36b
EugenS90 <66322185+EugenS90@users.noreply.github.com>
2020-10-18 00:40:25 +0300

    Add files via upload

--- src/bookmarks/Bookmarks.js
+++ src/bookmarks/Bookmarks.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Bookmarks.css';
+import logo from './logo.svg'
 
 class Bookmarks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div>
         <p>Bookmarks</p>
+        <div className='card'>
+          <p>BazaIT</p>
+          <img src={logo} />
+          <p>JS developer</p>
+        </div>
       </div>
     );
   }
